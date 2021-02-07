0f54fa1ddf0a3ed0f37b1bdaaa53eee352327186
smorales95 <77822216+smorales95@users.noreply.github.com>
2021-02-06 21:19:16 -0600

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function listview(dataview){
+   document.getElementById("viewwraplistview").style.display="none";
+   document.getElementById("viewwrapgridview").style.display="block";
+
+}
+
+function gridview(dataview){
+   document.getElementById("viewwraplistview").style.display="block";
+   document.getElementById("viewwrapgridview").style.display="none";
+
+}
